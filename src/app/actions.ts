e18919250fc75e3be79a1cd8ea584ec77ab13286
A1exMP <apalli.usc@gmail.com>
2024-08-06 11:19:43 -0500

    librerias
    librerias open ai y gemini

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,8 +1,23 @@
 import { Credentials, User } from '@/lib/types';
 import { createClient } from '@supabase/supabase-js'
+import { google } from '@ai-sdk/google';
+import { createOpenAI as createGroq } from '@ai-sdk/openai';
+import { generateText,experimental_createProviderRegistry as createProviderRegistry, generateObject, tool, streamText } from 'ai';
+import { z } from 'zod';
+import { GoogleGenerativeAI } from '@google/generative-ai';
+import { Ojuju } from 'next/font/google';
 const supabase = createClient('https://fsbkbozgyyfatbbbfcdj.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZzYmtib3pneXlmYXRiYmJmY2RqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI5MTk0NTMsImV4cCI6MjAzODQ5NTQ1M30.oYQ3Ha-5ORiZSeQgtv0px_UiJ6SgRU3qBNrk4__lcig')
+const genAI = new GoogleGenerativeAI(
+    process.env.GOOGLE_GENERATIVE_AI_API_KEY || ''
+)
 
-
+const registry = createProviderRegistry({
+    google,
+    /*groq: createGroq({
+        baseURL: 'https://api.groq.com/openai/v1',
+        apiKey: process.env.GROQ_API_KEY
+      }),*/
+});
 
 export async function Register(user: User) {
     const { data, error } = await supabase.auth.signUp({
@@ -46,3 +61,43 @@ export async function Login(user:Credentials) {
     return "Ha ocurrido un error " + error?.message
 }
 
+export async function GenerateText(params:any) {
+    const result = await generateText({
+        model: registry.languageModel('google:models/gemini-pro'),
+      //model:registry.languageModel('groq:llama-3.1-70b-versatile'),
+      //   maxTokens: 512,
+      //  temperature: 0.3,
+      //  maxRetries: 5,
+        prompt: ' genera  una  frase  con  huesos  para  el  flaco  del  alex ',
+      }); 
+      return result.text  
+}
+
+  
+export async function GernerateObject(params:any) {
+    const  object  = await generateObject({
+      model:registry.languageModel('groq:llama-3.1-70b-versatile'),
+      schema: z.object({
+        recipe: z.object({
+          name: z.string(),
+          temperature: z.array(z.object({ name: z.string(), amount: z.string() })),
+          steps: z.array(z.string()),
+        }),
+      }),
+      prompt: 'What is the weather like today?',
+      // @ts-ignore: Unreachable code error
+      tools: {
+          weather: tool({
+            description: 'Get the weather in a location',
+            parameters: z.object({
+              location: z.string().describe('The location to get the weather for'),
+            }),
+            execute: async ({ location }) => ({
+              location,
+              temperature: 72 + Math.floor(Math.random() * 21) - 10,
+            }),
+          }),
+        },
+    });
+   return object 
+}
